b32a467ee69c82caf3d57e17375d9c83f9d0c434
rustem bayetov <rustem.bayetov@gmail.com>
2019-11-17 14:44:57 -0600

    Kafka enabled

--- src/main/java/ea/notification/notification/controllers/EmailController.java
+++ src/main/java/ea/notification/notification/controllers/EmailController.java
@@ -1,6 +1,10 @@
 package ea.notification.notification.controllers;
 
+import com.google.gson.Gson;
+import ea.sof.shared.models.Answer;
+import ea.sof.shared.models.Question;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +21,19 @@ public class EmailController {
 
     @Autowired
     private JavaMailSender javaMailSender;
+
+    //Answer(id=1029, body=Answer about java, date=null, upvotes=0, topComments=[], userId=123, userName=rustem.bayetov@gmail.com)
+    @KafkaListener(topics = "${topicNewAnswer}", groupId = "${subsNewAnswerNotification}")
+    public void newQuestion(String message) {
+
+        System.out.println("New message from topic: " + message);
+
+        Gson gson = new Gson();
+        Answer answer =  gson.fromJson(message, Answer.class);
+
+        System.out.println("As object: " + answer);
+    }
+
     /* should be kafka listener
     * for now, make it parameteized post request
     * will get the question ID
